495c9e150ea361a95e0e27828f94c2ff64882a20
Muhammad Arslan <123346723+mecxlan@users.noreply.github.com>
2023-12-10 08:15:51 +0500

    Update 0_House_Price_Predictor.py

--- pages/0_House_Price_Predictor.py
+++ pages/0_House_Price_Predictor.py
@@ -39,6 +39,12 @@ st.markdown(
         - Check out [Hello World of Neural Network_tf](https://www.linkedin.com/pulse/hello-world-neural-network-tensorflow-muhammad-arslan-gnxrf/?trackingId=0hlhIzCCSRKAWuw5pu0DPA%3D%3D)
         - Follow me on [LinkedIn](www.linkedin.com/comm/mynetwork/discovery-see-all?usecase=PEOPLE_FOLLOWS&followMember=mecxlan)
 
+        
+
+        [Problem Statement](https://www.linkedin.com/posts/mecxlan_mecxlan-github-streamlit-activity-7138927999374057474-Qnht?utm_source=share&utm_medium=member_desktop):
+        A house has a base cost of 50k, and every additional bedroom adds a cost of 50k. This will make a 1-bedroom house cost 100k, a 2-bedroom house cost 150k etc.
+        How would you create a neural network that learns this relationship so that it would predict a 7-bedroom house as costing close to 400k etc.
+        Hint: Your network might work better if you scale the house price down. You don't have to give the answer 400...it might be better to create something that predicts the number 4, and then your answer is in the 'hundreds of thousands' etc.
       """
     )
 
